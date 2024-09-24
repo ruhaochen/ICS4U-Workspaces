d140d3d4e7774a2ba61b4fffc9bcbf2d615d8b43
Kevin Chen <rchen@bayviewglen.ca>
2024-09-24 14:41:17 -0400

    made classes and prototypes

---
+++ node-examples/objects-prototypes.js
@@ -0,0 +1,20 @@
+// let obj = {};
+
+// console.log(obj.__proto__);
+
+// let temp = '1';
+// console.log(temp.__proto__);
+
+// const arr = ['red', 'blue', 'green'];
+
+// console.log(arr);
+
+let person = {
+    name : "Steve",
+    age : 30,
+    isStudent : false
+}
+
+// console.log(person);
+
+// Object.getOwnPropertyNames(person.__proto__) is how to access the prototype in vs console
