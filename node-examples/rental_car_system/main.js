61d63502b2dec24ffa09c080c3cae678dea4687d
Kevin Chen <rchen@bayviewglen.ca>
2024-10-02 12:52:11 -0400

    fixed smth

--- node-examples/rental_car_system/main.js
+++ node-examples/rental_car_system/main.js
@@ -10,5 +10,5 @@ let car1 = new Car('Toyota', 'Camry', 2025, 20, true);
 let daysToRent = prompt('How many days do you want to rent this car? ')
 
 rentCar(car1, daysToRent);
-console.log(`The total price for renting the ${car1.make + " " + car1.model} for ${daysToRent} days is ${calculateRentalPrice(3, car1.pricePerDay)}`);
+console.log(`The total price for renting the ${car1.make + " " + car1.model} for ${daysToRent} days is ${calculateRentalPrice(daysToRent, car1.pricePerDay)}`);
 returnCar(car1);
